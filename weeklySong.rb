76c2a980aa064baafbb07d286e0f10d2ecb3fde7
Shirotazz <field.u8@gmail.com>
2017-06-04 20:19:44 +0900

    難易度の範囲指定に対応

--- weeklySong.rb
+++ weeklySong.rb
@@ -8,6 +8,8 @@ $songColumn = 1
 $songlist = []
 $cheer = ["頑張りましょう。", "えいえいおー！", "フルコンよゆーでしょ♪", "GOGO！", "がんばろ！", "ブシドー！"]
 $MAXLVL = "28"
+$MINLVL = "1"
+
 
 begin
   File.open('songlist') do |file|
@@ -22,13 +24,26 @@ bot.command(:kadai, description: "!kadai [difficulty]") do |event, *val|
     event.respond("refresh を実行してください。")
   end
 
+  difficulty = 1
+  count = 1
+
   # difficulty check
-  if val.empty? then
-    difficulty = "1"
-  else
-  
-  difficulty = val[0].to_i > $MAXLVL.to_i ? $MAXLVL : val[0].to_i
-  list = $songlist.select { |id, name, dif| dif.to_i >= difficulty.to_i}
+  case val.length
+  when 1
+    d = val[0].to_i
+    difficulty = d > $MAXLVL.to_i ? $MAXLVL : d
+  when 2, 3
+    if val.length == 3
+      c = val.pop.to_i
+      count = (0 < c && c < 10) ? c : 1
+    end
+    min = val.sort_by(&:to_i)[0]
+    max = val.sort_by(&:to_i)[1]
+    min = ($MINLVL < min && min < $MAXLVL) ? min : $MINLVL
+    max = ($MINLVL < max && max < $MAXLVL) ? max : $MAXLVL
+  end
+    
+  list = $songlist.select { |id, name, dif| (min <= dif.to_i && dif.to_i <= max) }
   event.respond("課題曲は#{list.sample[$songColumn]}です。 #{$cheer.sample}")
 end
 
